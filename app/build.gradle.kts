e969b936989b71e3507759edfb40975e41ea3a3f
Olga Molkova <olgamolkova190719@gmail.com>
2025-01-21 00:18:04 +0300

    add test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,12 +27,14 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+    implementation("net.datafaker:datafaker:2.0.1")
     implementation("org.instancio:instancio-junit:3.3.0")
     implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("jakarta.validation:jakarta.validation-api:3.1.0")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
     runtimeOnly("com.h2database:h2:2.2.224")
+
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
