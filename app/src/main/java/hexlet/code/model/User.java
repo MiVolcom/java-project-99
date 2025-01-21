0f9551ba2152cdf917d260b4ffdae9d7b7e358e9
Olga Molkova <olgamolkova190719@gmail.com>
2025-01-21 17:12:01 +0300

    step 2

---
+++ app/src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,56 @@
+package hexlet.code.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@ToString(includeFieldNames = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ToString.Include
+    @NotBlank
+    private String firstName;
+
+    @ToString.Include
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @ToString.Include
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+}
