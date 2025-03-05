1092d50ef5cd834391c7f6d4ef215ced18eaa517
MiVolcom <olgamolkova190719@gmail.com>
2025-03-05 23:55:18 +0300

    empty for test

--- src/main/java/hexlet/code/mapper/TaskMapper.java
+++ src/main/java/hexlet/code/mapper/TaskMapper.java
@@ -67,6 +67,7 @@ public abstract class TaskMapper {
             return null;
         }
 
+
         var assignee = userRepository.findById(assigneeId)
                 .orElseThrow(() -> new ResourceNotFoundException("User with id " + assigneeId + " not found"));
         return assignee;
